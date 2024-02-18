30239122d13bbb318079c2f3f7a7756bd9590f1b
Naiem Islam <naiemislam1547@gmail.com>
2024-02-18 13:06:28 +0600

    apply authenticate

--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from .models import Room, Topic
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.db.models import Q
 from .forms import RoomForm
 
@@ -10,6 +13,35 @@ from .forms import RoomForm
 #     {'id': 2, 'name':'Design With me'},
 #     {'id': 3, 'name':'frontend Developer'},
 # ]
+
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+
+        except:
+               messages.error(request, "User Doesn't exits")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+        else:
+            messages.error(request, 'Username and Password doesnot exits ')  
+
+    context = {}
+    return render(request, 'base/login_registration.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
